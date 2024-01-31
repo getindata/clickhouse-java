89956908715d6a45d998cbb43c18dae888b61904
Maciej Maciejko <maciej.maciejko@getindata.com>
2024-01-31 13:06:09 +0100

    refactor

--- clickhouse-http-client/src/main/java/com/clickhouse/client/gss/SubjectProvider.java
+++ clickhouse-http-client/src/main/java/com/clickhouse/client/gss/SubjectProvider.java
@@ -7,7 +7,7 @@ import javax.security.auth.Subject;
 class SubjectProvider {
 
     static Subject getSubject() {
-        return Subject.getSubject(AccessController.getContext());        // TODO add handling for java 17+
+        return Subject.getSubject(AccessController.getContext());        // TODO add handling for java 18+
     }
 
 }
